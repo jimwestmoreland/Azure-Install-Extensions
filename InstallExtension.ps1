b7482a11ff4db41f916c5f8f86f6eeb0378969e2
jimwestmoreland <jimwestmoreland@hotmail.com>
2018-09-28 11:47:43 -0500

    V1

---
+++ InstallExtension.ps1
@@ -0,0 +1,46 @@
+#Inputs Needed before you start
+#XML Template from Microsoft Doc
+#Storage Account
+#CSV file with Servername,Resourcegroup
+
+
+############################
+#
+#Manually Set the Following Variables
+#
+############################
+
+#Full Path to XML Template
+$diagnosticsconfig_path = "FULL PATH TO XML\NAME OF FILE.xml"
+
+#Specify the storage account
+$storageaccount = "ENTER STORAGE ACCOUNT"
+
+#############################
+#Import CSV Into Array
+#############################
+$array =Import-Csv -Path "C:\users\jimwest\Desktop\vmstlist.csv" -Delimiter ","
+
+
+$array | ForEach-Object {
+    $vm_name = $_.ServerName
+    $vm_resourcegroup = $_.ResourceGroup
+    
+    #############################
+    #Get the ResourceID for the VM to update XML
+    #############################
+    #Get the Resource ID
+    $resourceID = get-azurermresource -name <$vm_name> | ft -property resourceid
+
+    ##############################
+    #Update the XML template
+    ##############################
+    [xml]$myXML = Get-Content $diagnosticsconfig_path
+    $myXML.PublicConfig.WadCfg.DiagnosticMonitorConfiguration.Metrics.resourceid = $resourceid
+    $myxml.PublicConfig.StorageAccount = $storageaccount
+    $myXML.Save("$diagnosticsconfig_path")
+
+    Set-AzureRmVMDiagnosticsExtension -ResourceGroupName $vm_resourcegroup -VMName $vm_name -DiagnosticsConfigurationPath $diagnosticsconfig_path
+
+    Write-Host "servername is " $VM " and ResourceGroup is " $RG
+   }
